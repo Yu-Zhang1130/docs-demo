20b8161c75df59bd51ee256df3c776c98af185ed
Yu-Zhang1130 <17311303459@.163.com>
2026-04-17 09:52:32 +0800

    first commit

---
+++ .vitepress/config.mjs
@@ -0,0 +1,70 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  head: [["link", { rel: "icon", href: "/photo/章鱼小丸子.png" }]],
+  title: "我的项目",
+  description: "A VitePress Site",
+  themeConfig: {
+       // 设置搜索框的样式
+    search: {
+      provider: "local",
+      options: {
+        translations: {
+          button: {
+            buttonText: "搜索文档",
+            buttonAriaLabel: "搜索文档",
+          },
+          modal: {
+            noResultsText: "无法找到相关结果",
+            resetButtonTitle: "清除查询条件",
+            footer: {
+              selectText: "选择",
+              navigateText: "切换",
+            },
+          },
+        },
+      },
+    },
+    sidebar: "false",//关闭侧边栏
+    aside: "left",//设置右侧侧边栏在左侧显示
+    outlineTitle: "目录",
+    outline: [2,6],
+    logo: "/photo/zhangyu.png",
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: '家', items:[
+        { text: '首页', link: '/' },
+        { text: 'markdown示例', link: '/markdown-examples' }
+      ]
+        },
+      { text: '程序',
+        items: [
+          { text: 'C/C++', link: '/C/C++-examples' },
+          { text: 'FPGA', link: '/FPGA-examples' }
+        ]},
+      
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+    sidebar: false,//关闭侧边栏
+    aside: "left",//设置右侧侧边栏在左侧显示
+    
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ],
+    footer:{
+copyright: "Copyright@ 2023 Yu_Zhang",
+    },
+  },
+  
+});
+
---
+++ index.md
@@ -0,0 +1,32 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "干净🐟的秘密基地"
+  text: "健康小程序"
+  tagline: My great project tagline
+  image: 
+    src: /photo/zhangyu.png
+    alt: 背景图片
+  actions:
+  #action是按钮 
+    - theme: brand
+      text: Markdown Examples
+      #内容
+      link: /markdown-examples
+      #跳到哪
+    - theme: alt
+      text: API 实例
+      link: /api-examples
+      #api-examples.md
+
+features:
+  - title: Feature A
+    details: 特征1的详细描述
+  - title: Feature B
+    details: 特征2的详细描述
+  - title: Feature C
+    details: 特征3的详细描述
+---
+
